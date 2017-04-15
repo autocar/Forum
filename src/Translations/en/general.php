47d471dda3aa3beeeacb249fb59dc1bf9c74aa08
24aitor <contact@aitorriba.com>
2017-04-15 02:29:54 +0200

    Added Translations

--- src/Translations/en/general.php
+++ src/Translations/en/general.php
@@ -34,7 +34,6 @@ return [
     'category_updated'  => "Category #:id updated!",
     'category_deleted'  => "Category #:id deleted!",
 
-
     'edit_category'   => 'Edit Category',
     'create_category' => 'Create Category',
     'categories_desc' => 'Categories will allow you to separate threads into independent sections.',
@@ -42,7 +41,6 @@ return [
     'category_list'   => 'Category List',
     'create_category_desc' => 'Create a new category to the database',
     'edit_category_desc'   => "You're editing category #:id created :time_ago",
-
 
     'thread'           => 'Thread',
     'view_thread'      => 'View Thread',
@@ -55,14 +53,12 @@ return [
     'thread_updated'   => "Thread #:id updated!",
     'thread_deleted'   => "Thread #:id deleted!",
 
-
     'edit_thread'         => 'Edit Thread',
     'create_thread'       => 'Create Thread',
     'categories_desc'   => 'Categories will allow you to separate threads into independent sections.',
     'categories'        => 'Categories',
     'create_thread_desc'  => 'Create a new thread to the database',
     'edit_thread_desc'    => "You're editing thread #:id created :time_ago",
-
 
     'written_by'        => 'Written by :username :time_ago. Threaded in :cat category.',
     'comments_choice'   => ':num comment|:num comments',
@@ -75,7 +71,6 @@ return [
     'add_a_comment'     => 'Add a public comment...',
     'edit_a_comment'    => 'Edit this comment...',
     'comment_added'     => 'Comment added!',
-
 
     'sure_del_category' => 'Are you sure you want to delete category ":category" ?',
     'sure_del_thread'     => 'Are you sure you want to delete thread ":thread" ?',
